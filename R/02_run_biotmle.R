734d5a4a8ff0624faeafc2c5b2bae1e78804ca53
Nima Hejazi <nh@nimahejazi.org>
2018-11-07 14:22:46 -0800

    update visualizations

--- R/02_run_biotmle.R
+++ R/02_run_biotmle.R
@@ -1,7 +1,7 @@
 library(biotmle)
 g_lib = c("SL.mean", "SL.speedglm")
 Q_lib = c("SL.mean", "SL.speedglm")
-biotmle_results <- biomarkertmle(se = se[1:100,], varInt = 1, family = "gaussian",
+biotmle_results <- biomarkertmle(se = se[1:250,], varInt = 1, family = "gaussian",
                                  g_lib = g_lib, Q_lib = Q_lib, parallel = TRUE
                                 )
 eif_test <- modtest_ic(biotmle = biotmle_results)
--- R/03_vis_results.R
+++ R/03_vis_results.R
@@ -2,12 +2,18 @@ plot(x = eif_test, type = "pvals_adj")
 
 plot(x = eif_test, type = "pvals_raw")
 
+pdf(file = here("figs", "supervised_heatmap.pdf"))
 heatmap_ic(x = eif_test,
            clustering.method = "hierarchical",
            left.label = "none",
-           smooth.heat = TRUE,
+           #bottom.label = "variable",
+           #col.dendrogram = TRUE,
+           pretty.order.rows = TRUE,
+           pretty.order.cols = TRUE,
+           #smooth.heat = TRUE,
            scale = TRUE,
-           design = as.numeric(A == max(A)),
-           FDRcutoff = 1,
-           top = nrow(eif_test))
+           design = as.numeric(A == min(A)),
+           FDRcutoff = 0.2,
+           top = 50)
+dev.off()
 
